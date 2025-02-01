fec3acd16735d8f1ffe0bf33d9d4d2fea64a8a61
Rom-q <golmanromy@gmail.com>
2025-02-01 19:59:56 +0300

    new file:   Dotatranslate2/Dotatranslate2.csproj 	new file:   Dotatranslate2/Program.cs 	new file:   Dotatranslate2/obj/Debug/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs 	new file:   Dotatranslate2/obj/Debug/net8.0/Dotatranslate2.AssemblyInfo.cs 	new file:   Dotatranslate2/obj/Debug/net8.0/Dotatranslate2.AssemblyInfoInputs.cache 	new file:   Dotatranslate2/obj/Debug/net8.0/Dotatranslate2.GeneratedMSBuildEditorConfig.editorconfig 	new file:   Dotatranslate2/obj/Debug/net8.0/Dotatranslate2.GlobalUsings.g.cs 	new file:   Dotatranslate2/obj/Debug/net8.0/Dotatranslate2.assets.cache 	new file:   Dotatranslate2/obj/Dotatranslate2.csproj.nuget.dgspec.json 	new file:   Dotatranslate2/obj/Dotatranslate2.csproj.nuget.g.props 	new file:   Dotatranslate2/obj/Dotatranslate2.csproj.nuget.g.targets 	new file:   Dotatranslate2/obj/project.assets.json 	new file:   Dotatranslate2/obj/project.nuget.cache 	modified:   dotatest.sln 	modified:   dotatest/obj/Debug/net8.0/dotatest.AssemblyInfo.cs 	modified:   dotatest/obj/Debug/net8.0/dotatest.AssemblyInfoInputs.cache 	modified:   dotatest/obj/Debug/net8.0/dotatest.GeneratedMSBuildEditorConfig.editorconfig 	modified:   dotatest/obj/Debug/net8.0/dotatest.assets.cache

--- Dotatranslate2/obj/Debug/net8.0/Dotatranslate2.AssemblyInfo.cs
+++ Dotatranslate2/obj/Debug/net8.0/Dotatranslate2.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("dotatest")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Dotatranslate2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db41c4e9696663cdc79d18b797b0f804857e94d4")]
-[assembly: System.Reflection.AssemblyProductAttribute("dotatest")]
-[assembly: System.Reflection.AssemblyTitleAttribute("dotatest")]
+[assembly: System.Reflection.AssemblyProductAttribute("Dotatranslate2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Dotatranslate2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
